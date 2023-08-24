7e12e1dee983679f4447c870f5d5522e2ff1e94e
IgorPushka <igor081991@yandex.ru>
2023-08-24 18:49:11 +0300

    sprint 10.1-1 Оптимизированы импорты в классах Film, User

--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -10,7 +10,6 @@ import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.PastOrPresent;
 import java.time.LocalDate;
-import java.util.ArrayList;
 import java.util.List;
 
 @Data
